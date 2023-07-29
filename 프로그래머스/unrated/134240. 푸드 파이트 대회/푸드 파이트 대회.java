69b062ae505649b7d0ddb86f33c35d663750af4b
Somin Oh <121929230+o-dim@users.noreply.github.com>
2023-07-29 16:07:44 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 10.08 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+        StringBuffer answer = new StringBuffer();
+        for(int i = 1; i < food.length; i++) {
+            if(food[i] % 2 == 1) {
+                food[i] = food[i] - 1;
+            }
+            for(int j = 1; j <= food[i] / 2; j++) {
+                answer.append(String.valueOf(i));
+            }
+        }
+        String str = answer.toString() + "0" + answer.reverse().toString();
+
+        return str;
+
+    }
+}
