c2bd018874041442298766acc1ee05b04e570a27
Somin Oh <121929230+o-dim@users.noreply.github.com>
2023-07-31 15:20:20 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        StringBuilder sb = new StringBuilder();
-        for(char str : myString.toCharArray()) {
-            if(str == 'A') {
-                sb.append('B');
-            } else if(str == 'B') {
-                sb.append('A');
-            }
-        }
-        return sb.toString().contains(pat) == true? 1 : 0;
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) == true? 1 : 0;
     }
 }
